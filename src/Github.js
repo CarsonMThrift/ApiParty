d7dfbbe3e0b31e293069ea51989b12456f348f99
Carson Thrift <Carson Thrift>
2017-06-20 11:52:51 -0400

    add submit handler and push to new history location

--- src/Github.js
+++ src/Github.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import "./Github.css"
 
 class Github extends Component {
@@ -12,11 +13,17 @@ class Github extends Component {
         this.setState({ username })
     }
 
+    handleSubmit = (ev) => {
+        ev.preventDefault()
+        this.props.history.push('/github/${this.state/username}')
+
+    }
+
     render() {
         return (
             <div className="github">
                 <img src="http://www.aha.io/assets/github.7433692cabbfa132f34adb034e7909fa.png" alt="github logo" className="github-logo" />
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <div>
                         <input
                             type="text"
@@ -28,6 +35,7 @@ class Github extends Component {
                         <button type="submit">Lookup Github user</button>
                     </div>
                 </form>
+                <Route exact path='/github' render={() => <h3>Please enter a username to search on Github</h3>} />
             </div>
         )
     }
